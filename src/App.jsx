ec6f3e03ca30eba1d8beeda31a859b26861c790a
catherinrojas-star <catherin.rojas@anima.edu.uy>
2025-11-06 20:36:50 -0300

    Revert "Merge branch 'main' into Catherinrojas-"
    This reverts commit 82f062bee8ae0c39519455ac874210bbc69bcead, reversing
    changes made to b005b48269236db1d20e6972a48e2858000ad1ef.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieNavbar from "./components/Navbar";
-import Peliculas from "./components/Peliculas";
-import Banner from "./components/Banner";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <MovieNavbar />
-      <Banner />
-      <Routes>
-        <Route path="/" element={<Peliculas />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <p>Hola</p>
+      </div>
+    </>
   );
 }
+
 export default App;
