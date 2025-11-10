10bd44c91a509d658e9ed6a323a5af09b478e5a8
FiorellaOrdenesss <fiordenesvt@gmail.com>
2025-11-10 19:54:45 -0300

    Inicio desde cero

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MovieNavbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Peliculas from "./components/Peliculas";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <MovieNavbar />
+        <Banner />
+        <Routes>
+          <Route path="/" element={<Peliculas />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
