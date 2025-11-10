a56cd0b2455105e73f2c573b79d56855222a37cd
FiorellaOrdenesss <fiordenesvt@gmail.com>
2025-11-10 20:44:28 -0300

    cambios en app

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,9 @@ import Peliculas from "./components/Peliculas";
 const App = () => {
   return (
     <div>
-      <h1 style={styles.titulo}>Hackflix 🎬</h1>
       <Peliculas />
     </div>
   );
-};
-
-const styles = {
-  titulo: {
-    textAlign: "center",
-    fontSize: "2.5rem",
-    margin: "20px 0",
-    color: "#333",
-  },
 };
 
 export default App;
